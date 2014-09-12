dee16bb677cf94d6c31869be130c017896981d0a
Margaret Godowns <margaretgodowns@gmail.com>
2014-09-12 09:02:25 -0400

    reviews work

--- js/localProducts/localProductsSvc.js
+++ js/localProducts/localProductsSvc.js
@@ -7,25 +7,29 @@ angular.module("localProducts")
       name: "Glasgow",
       price: 25,
       image: "http://images.cdn.bigcartel.com/bigcartel/product_images/142456717/max_h-1000+max_w-1000/IMG_0844.JPG",
-      description: "The Glasgow Head wrap is inspired by the rich tartans of Scotland."
+      description: "The Glasgow Head wrap is inspired by the rich tartans of Scotland.",
+      reviews: []
     },
     {
       name: "Arusha",
       price: 25,
       image: "http://images.cdn.bigcartel.com/bigcartel/product_images/142456639/max_h-1000+max_w-1000/IMG_0849.JPG",
-      description: "Inspired by the bustling streets of Arusha in Eastern Africa, this fabric is vintage and a limited edition piece this Fall."
+      description: "Inspired by the bustling streets of Arusha in Eastern Africa, this fabric is vintage and a limited edition piece this Fall.",
+      reviews: []
     },
     {
       name: "Rio",
       price: 15,
       image: "http://images.cdn.bigcartel.com/bigcartel/product_images/142457386/max_h-1000+max_w-1000/IMG_0858.JPG",
-      description: "Inspired by Brazil, the worlds leading producer of coffee beans. This chunky knit brown and white striped turban has me thinking café com leite!"
+      description: "Inspired by Brazil, the worlds leading producer of coffee beans. This chunky knit brown and white striped turban has me thinking café com leite!",
+      reviews: []
     },
     {
       name: "Copenhagen",
       price: 50,
       image: "http://images.cdn.bigcartel.com/bigcartel/product_images/142457443/max_h-1000+max_w-1000/IMG_0866.JPG",
-      description: "Large cream roses on a satin ribbon. Simplicity as easy as it comes without sacrificing quality."
+      description: "Large cream roses on a satin ribbon. Simplicity as easy as it comes without sacrificing quality.",
+      reviews: []
     }
     ];
 
@@ -39,7 +43,7 @@ angular.module("localProducts")
 
     var addReview = function (idx, review) {
       _inventory[idx].reviews.push(review);
-     }
+    };
 
     // var addProduct = function(newProduct){
     //   _inventory.push(newProduct);
